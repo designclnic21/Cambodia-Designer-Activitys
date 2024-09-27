80ebbe070b8fb2814d2a041f11fa65f0c8b83bd6
designclnic21 <team.designc21@gmail.com>
2024-09-27 10:42:09 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+// script.js
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function changeSlide(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i
